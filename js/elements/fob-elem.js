73ed5e7a34e064d784db38808ef195b7d7042277
Oleksyii Mihno <xioflesh@gmail.com>
2022-05-20 12:18:56 +0300

    move target drop and line in fob js

--- js/elements/fob-elem.js
+++ js/elements/fob-elem.js
@@ -5,6 +5,14 @@ export default class FobElement extends HTMLElement {
         // this.dataset.fobId = count;
         // console.log(`Create element FOB ${this.dataset.fobId++}`);
         // this.firstSplit = 
+       this.drag = new PlainDraggable(this, {
+            // handle: document.querySelector('#fob-2 .draggable'),
+            onMove: function() { line.position(); },
+            // onMoveStart: function() { line.dash = {animation: true}; },
+            // onDragEnd: function() { line.dash = false; },
+            // handle: true,
+            zIndex: 1
+          });
     }
 
     connectedCallback() {
@@ -26,50 +34,62 @@ export default class FobElement extends HTMLElement {
         const target = event.target;
         
         if (target.matches('.create-fob')) {
-            let fob = document.createElement('fob-element')
+            let fob = document.createElement('fob-element'),
+                fobHead = document.createElement('div');
+
+                fob.classList.add('fob');
+
+                fobHead.classList.add('fob-head', 'draggable');
+                fobHead.textContent = 'Drag This';
+
+                fob.append(fobHead);
+
+            
             console.log(`FOB was clicked! ${this.id}`);
-            this.append(fob)
+            console.log();
+            field.append(fob)
 
 
-            let target1 = document.getElementById('fob-1'),
-            target2 = document.getElementById('fob-2'),
-            wallBBox = document.getElementById('field').getBoundingClientRect(),
+            let btnCreateNewFob = target,
+            newFob = fob,
+            // wallBBox = document.getElementById('field').getBoundingClientRect(),
             line;
+            // new PlainDraggable(btnCreateNewFob, {
+            //     // onDrag: function(moveTo) {
+            //     //   let rect = this.rect;
+            //     //   if (moveTo.left < wallBBox.right && moveTo.top > wallBBox.top) { // Inside
+            //     //     if (rect.left >= wallBBox.right) {
+            //     //       moveTo.left = wallBBox.right;
+            //     //     } else if (rect.top <= wallBBox.top) { // Avoid fixing both left and top.
+            //     //       moveTo.top = wallBBox.top;
+            //     //     }
+            //     //   }
+            //     // },
+            //     // handle: document.querySelector('#fob-1 .draggable'),
+            //     // onMove: function() { line.position(); },
+            //     // onMoveStart: function() { line.dash = {animation: true}; },
+            //     // onDragEnd: function() { line.dash = false; },
+                
+            //     // handle: true,
+            //     zIndex: 1
+            //   });
+            
+              new PlainDraggable(newFob, {
+                handle: document.querySelector('#fob-2 .draggable'),
+                onMove: function() { line.position(); },
+                // onMoveStart: function() { line.dash = {animation: true}; },
+                // onDragEnd: function() { line.dash = false; },
+                // handle: true,
+                zIndex: 1
+              });
+            
+              line = new LeaderLine(btnCreateNewFob, newFob);
 
 
         }
 
-        new PlainDraggable(target1, {
-            // onDrag: function(moveTo) {
-            //   let rect = this.rect;
-            //   if (moveTo.left < wallBBox.right && moveTo.top > wallBBox.top) { // Inside
-            //     if (rect.left >= wallBBox.right) {
-            //       moveTo.left = wallBBox.right;
-            //     } else if (rect.top <= wallBBox.top) { // Avoid fixing both left and top.
-            //       moveTo.top = wallBBox.top;
-            //     }
-            //   }
-            // },
-            handle: document.querySelector('#fob-1 .draggable'),
-            onMove: function() { line.position(); },
-            // onMoveStart: function() { line.dash = {animation: true}; },
-            // onDragEnd: function() { line.dash = false; },
-            
-            // handle: true,
-            zIndex: 1
-          });
-        
-          new PlainDraggable(target2, {
-            handle: document.querySelector('#fob-2 .draggable'),
-            onMove: function() { line.position(); },
-            // onMoveStart: function() { line.dash = {animation: true}; },
-            // onDragEnd: function() { line.dash = false; },
-            // handle: true,
-            zIndex: 1
-          });
-        
-          line = new LeaderLine(target1, target2);
-          
+
+
         // console.log('event: ', this.event.target);
         // let target = event.target
         // console.log('event: ', target.matches('.create-split'));
