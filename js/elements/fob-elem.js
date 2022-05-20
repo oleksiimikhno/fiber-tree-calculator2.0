7a6e455cf4ca20e9a06e78fcffc831169120d074
Oleksyii Mihno <xioflesh@gmail.com>
2022-05-20 15:57:59 +0300

    add line out split to next fob

--- js/elements/fob-elem.js
+++ js/elements/fob-elem.js
@@ -5,19 +5,12 @@ export default class FobElement extends HTMLElement {
         // this.dataset.fobId = count;
         // console.log(`Create element FOB ${this.dataset.fobId++}`);
         // this.firstSplit = 
-       this.drag = new PlainDraggable(this, {
-            // handle: document.querySelector('#fob-2 .draggable'),
-            onMove: function() { line.position(); },
-            // onMoveStart: function() { line.dash = {animation: true}; },
-            // onDragEnd: function() { line.dash = false; },
-            // handle: true,
-            zIndex: 1
-          });
+        this.mouseDownOnThis = false;
     }
 
     connectedCallback() {
         console.log('Fob element in DOM');
-        // this._render();
+        this._render();
         this._addEventListeners();
     }
 
@@ -26,8 +19,23 @@ export default class FobElement extends HTMLElement {
         this._removeEventListeners();
     }
 
+    _render() {
+        this.drag = new PlainDraggable(this, {
+            handle: this.querySelector('.draggable'),
+            // onMove: function() { line.position(); },
+            // onMoveStart: function() { line.dash = {animation: true}; },
+            // onDragEnd: function() { line.dash = false; },
+            // handle: true,
+            zIndex: 1
+          });
+
+          console.log(this.line);
+    }
+
     _addEventListeners() {
-        this.addEventListener('click', this.createFob);
+        let ww = this.addEventListener('click', this.createFob);
+        console.log('ww: ', ww);
+        console.log(this.arrayLine);
     }
 
     createFob(event) {
@@ -37,66 +45,60 @@ export default class FobElement extends HTMLElement {
             let fob = document.createElement('fob-element'),
                 fobHead = document.createElement('div');
 
-                fob.classList.add('fob');
-
-                fobHead.classList.add('fob-head', 'draggable');
-                fobHead.textContent = 'Drag This';
-
-                fob.append(fobHead);
-
+            fob.classList.add('fob');
+            fobHead.classList.add('fob-head', 'draggable');
+            fobHead.textContent = 'Drag This 2';
+            fob.append(fobHead);
             
-            console.log(`FOB was clicked! ${this.id}`);
-            console.log();
-            field.append(fob)
 
-
-            let btnCreateNewFob = target,
-            newFob = fob,
-            // wallBBox = document.getElementById('field').getBoundingClientRect(),
-            line;
-            // new PlainDraggable(btnCreateNewFob, {
-            //     // onDrag: function(moveTo) {
-            //     //   let rect = this.rect;
-            //     //   if (moveTo.left < wallBBox.right && moveTo.top > wallBBox.top) { // Inside
-            //     //     if (rect.left >= wallBBox.right) {
-            //     //       moveTo.left = wallBBox.right;
-            //     //     } else if (rect.top <= wallBBox.top) { // Avoid fixing both left and top.
-            //     //       moveTo.top = wallBBox.top;
-            //     //     }
-            //     //   }
-            //     // },
-            //     // handle: document.querySelector('#fob-1 .draggable'),
-            //     // onMove: function() { line.position(); },
-            //     // onMoveStart: function() { line.dash = {animation: true}; },
-            //     // onDragEnd: function() { line.dash = false; },
+            let firstSplit = document.createElement('split-element');
+            firstSplit.classList.add('row', 'split');
+            // firstSplit.id = `#split-${i}`;
                 
-            //     // handle: true,
-            //     zIndex: 1
-            //   });
+
+            field.append(fob);
             
-              new PlainDraggable(newFob, {
-                handle: document.querySelector('#fob-2 .draggable'),
+            firstSplit.innerHTML  = 
+                `<div class="row split-selected">
+                    <select class="select-type">
+                        <option value="FBT">FBT</option>
+                        <option value="PLC">PLC</option>
+                    </select>
+                    <select class="select-fiber">
+                        <option value="5_95">5/95</option>
+                        <option value="10_90">10/90</option>
+                        <option value="15_85">15/85</option>
+                    </select>
+                </div>
+                <div class="vertical-center"><input class="in-split" name="in-signal" value="0"></div>
+                <div class="column out-split">
+                    <div class="split-out row" data-id="0">
+                        <input class="out-signal" data-id="0" name="out-split" value="0" disabled="">
+                        <button class="btn-split create-split">+</button>
+                        <svg class="icon icon-out green create-fob"><use xlink:href="icon/icon.symbol.svg#arrow-right-line"></use></svg>
+                    </div>
+                    <div class="split-out row" data-id="0">
+                        <input class="out-signal" data-id="0" name="out-split" value="0" disabled="">
+                        <button class="btn-split create-split">+</button>
+                        <svg class="icon icon-out green create-fob"><use xlink:href="icon/icon.symbol.svg#arrow-right-line"></use></svg>
+                    </div>
+                </div>`;
+
+            fob.insertAdjacentElement('beforeend', firstSplit);
+
+            let line ;
+            new PlainDraggable(fob, {
+                handle: fob.querySelector('.draggable'),
                 onMove: function() { line.position(); },
-                // onMoveStart: function() { line.dash = {animation: true}; },
-                // onDragEnd: function() { line.dash = false; },
-                // handle: true,
                 zIndex: 1
-              });
-            
-              line = new LeaderLine(btnCreateNewFob, newFob);
-
-
-        }
-
-
-
-        // console.log('event: ', this.event.target);
-        // let target = event.target
-        // console.log('event: ', target.matches('.create-split'));
+            });
         
-    } 
+            line = new LeaderLine(target, fob.querySelector('[name="in-signal"]'));
 
-
+            const watchMoveThisFob = field.addEventListener('mousemove', AnimEvent.add(function() {
+                line.position();}), false);
+        }
+    }
   }
 
   customElements.define("fob-element", FobElement);
