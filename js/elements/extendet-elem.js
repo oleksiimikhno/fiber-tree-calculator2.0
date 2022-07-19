369dceae561a7f5627452b451858a47b318c366a
Alex <xioflesh@gmail.com>
2022-07-19 15:34:44 +0300

    add methods update/remove line. removEventList

--- js/elements/extendet-elem.js
+++ js/elements/extendet-elem.js
@@ -1,10 +1,10 @@
 
 
 export default class ExtendetHTMLElement extends HTMLElement {
-    constructor(name) {
+    constructor() {
         super();
-        this.elementLine;
-        this.rect = this.getBoundingClientRect();
+        this.line;
+        this.prevElement;
     }
 
     connectedCallback() {
@@ -20,8 +20,12 @@ export default class ExtendetHTMLElement extends HTMLElement {
     }
 
     _addEventListeners() {
-        this.addEventListener('click', this.createFob);
-        this.querySelector('.draggable').addEventListener('mousemove', this.updateSplitLinePosition);
+    }
+
+    _removeEventListeners() {
+        console.log(this.prevElement);
+        console.log(123);
+        this.prevElement.removeEventListener('mousemove', AnimEvent.add(() => line.position()), false);
     }
 
     onDraggableElement(element) {
@@ -34,16 +38,25 @@ export default class ExtendetHTMLElement extends HTMLElement {
     }
 
     createLine(prevElement, target, newElement, color = '#3197fd', startSocket = 'auto') {
-        let line = new LeaderLine(target, newElement);
+        this.prevElement = prevElement.parentElement;
+
+        const line = new LeaderLine(target, newElement);
         line.setOptions({startSocket: startSocket, endSocket: 'left'});
         line.setOptions({path: 'grid'});
         line.setOptions({color: color});
 
-        const updateLinePosition = prevElement.parentElement.addEventListener('mousemove', AnimEvent.add(function() {
-            line.position()
-        }), false);
+        this.updateLinePosition(this.prevElement, line);
 
         return line;
+    }
+
+    updateLinePosition(element, line) {
+        console.log('element: ', element);
+        element.addEventListener('mousemove', AnimEvent.add(() => line.position()), false);
+    }
+
+    removeLine(line) {
+        line.remove();
     }
 
     getTranslateXY(element) {
@@ -54,24 +67,4 @@ export default class ExtendetHTMLElement extends HTMLElement {
             translateY: matrix.m42
         }
     }
-    
-
-    // createLine(targetnewFob) {
-    //     let line;
-    //     let drag = new PlainDraggable(newFob, {
-    //         handle: newFob.querySelector('.draggable'),
-    //         onMove: function() { line.position(); },
-    //         zIndex: 1
-    //     });
-    
-    //     line = new LeaderLine(target, targetnewFob);
-    //     line.setOptions({startSocket: 'right', endSocket: 'left'});
-    //     line.setOptions({color: '#3197fd'});
-
-    //     const updatePisitionThisFob = field.addEventListener('mousemove', AnimEvent.add(function() {
-    //         line.position();
-    //     }), false);
-
-    // }
-
 }
